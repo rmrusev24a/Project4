f987fc33532b95abb3c41fc7d703485e1efee8e6
rmrusev24a <rmrusev24a@gmail.com>
2026-01-22 10:47:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 st.title("Логин система")
 name = st.text_input("Въведи име")
+age = st.number_input("Въведи години")
 if st.button("Провери"):
     if name.strip() == "":
         st.warning("Моля въведи текст")
     elif not name.isalpha():
         st.warning("Името трябва да съдържа само букви")
+     elif age < 18:
+        st.error("Трябва да имаш навършени 18 години")
     else:
-        st.success("Текстът е въведен правилно")
+        st.success("Достъпът е разрешен. Добре дошъл!")
