dc0d698ff693e21fcd0e47c69f4b2fe8fe8a02b4
rmrusev24a <rmrusev24a@gmail.com>
2026-01-22 10:32:55 +0200

    Fix login input validation and messages

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
+
 st.title("Логин система")
 name = st.text_input("Въведи име")
-  if st.button("Провери")
-    if name.strip ()=="":
-      st.warning("Моля въведи текст")
-  elif not name is alpha():
-  st.warning("----")
-    else st.success("Текстът е въведен правилно")
+if st.button("Провери"):
+    if name.strip() == "":
+        st.warning("Моля въведи текст")
+    elif not name.isalpha():
+        st.warning("Името трябва да съдържа само букви")
+    else:
+        st.success("Текстът е въведен правилно")
